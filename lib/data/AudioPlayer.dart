e92779b62d83bc0b27dd94aaa94e81ed8f0c929f
Thomas Baker <thomas_baker@intuit.com>
2023-11-12 07:37:55 -0800

    Slight player refactor to keep sound working running on mobile.

--- lib/data/AudioPlayer.dart
+++ lib/data/AudioPlayer.dart
@@ -1,13 +1,17 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
 
 class Player {
-  static var _player = AudioPlayer();
+  static final _clickSource = AssetSource('sounds/click.mp3');
+  static final _startSource = AssetSource('sounds/start.mp3');
 
   static playClick() async {
-    await _player.play(AssetSource('sounds/click.mp3'));
+    await AudioPlayer().play(_clickSource, mode: PlayerMode.lowLatency);
+    debugPrint("Playing click sound");
   }
 
   static playStart() async {
-    await _player.play(AssetSource('sounds/start.mp3'));
+    await AudioPlayer().play(_startSource, mode: PlayerMode.lowLatency);
+    debugPrint("Playing start sound");
   }
 }
